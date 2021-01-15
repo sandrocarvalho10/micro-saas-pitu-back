bc6b328d03f847814a3ccc17f0f710cdd6684c24
Sandro Carvalho <sandrinho9797@outlook.com>
2021-01-15 13:39:02 -0300

    config Mysql

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 // 'mysql://root:teste1234@localhost:3306/micro_saas_pitu'
-//mysql://epiz_27695047:5oQv3y97F3kPxP@sql109.epizy.com:3306/epiz_27695047_micro_saas_pitu_db
+mysql://epiz_27695047:5oQv3y97F3kPxP@sql109.epizy.com:3306/epiz_27695047_micro_saas_pitu_db
 
-const sequelize = new Sequelize('mysql://root:teste1234@localhost:3306/micro_saas_pitu');
+const sequelize = new Sequelize('mysql://epiz_27695047:5oQv3y97F3kPxP@sql109.epizy.com:3306/epiz_27695047_micro_saas_pitu_db');
 
 
 export default sequelize;
