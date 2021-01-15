08193399a43463b144288a730efd75f2a7872b68
Sandro Carvalho <sandrinho9797@outlook.com>
2021-01-15 10:15:12 -0300

    config Mysql Infinity Free

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('mysql://epiz_27695047:teste1234@sql109.epizy.com:3306/epiz_27695047_micro_saas_pitu_db'||'mysql://root:teste1234@localhost:3306/micro_saas_pitu');
+// 'mysql://root:teste1234@localhost:3306/micro_saas_pitu'
+const sequelize = new Sequelize('mysql://epiz_27695047:teste1234@sql109.epizy.com:3306/epiz_27695047_micro_saas_pitu_db');
 
 export default sequelize;
 
